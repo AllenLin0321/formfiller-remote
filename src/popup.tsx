3641460d1e9cd9fd8c794d20ee96f6a534c7112e
林逸倫 <glux7381@gmail.com>
2025-05-08 22:59:30 +0800

    Add Redirect Function

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,13 +1,23 @@
-import { CountButton } from "~features/count-button"
+import { useEffect } from "react"
 
-import "~style.css"
+const Popup = () => {
+  useEffect(() => {
+    // 找出當前分頁並替換成 ChatGPT
+    chrome.tabs
+      .query({ active: true, currentWindow: true })
+      .then((tabs) => {
+        if (tabs[0]?.id != null) {
+          chrome.tabs.update(tabs[0].id, {
+            url: "https://docs.google.com/forms/d/e/1FAIpQLSf51MQ7zTzo_AMy7JhUHvP0En_y3LJNL4DgSGpNsdCxUC_vLg/viewform?usp=pp_url&entry.1923207762=%E9%81%B8%E9%A0%85+1"
+          })
+        }
+      })
+      .finally(() => {
+        window.close() // 關閉 popup
+      })
+  }, [])
 
-function IndexPopup() {
-  return (
-    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
-      <CountButton />
-    </div>
-  )
+  return <div></div>
 }
 
-export default IndexPopup
+export default Popup
